c678721e7fdbe7e2b36f93207bbbc8688ade1cbb
sudheer <sudheer.net2020@gmail.com>
2024-03-22 18:36:20 +0530

    episode-o1

---
+++ App.js
@@ -0,0 +1,17 @@
+// const heading = React.createElement("h1",{id:"heading"},"hellow world from react");
+
+// const root =ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+const parent = React.createElement("div",{id:"parent"},
+[React.createElement("div",{id:"child1"},
+[React.createElement("h1",{},"IAM h1 tag"),
+React.createElement("h2",{},"IAM h2 tag")]),
+
+React.createElement("div",{id:"child2"},
+[React.createElement("h1",{},"IAM h1 tag"),
+React.createElement("h2",{},"IAM h2 tag")])]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
